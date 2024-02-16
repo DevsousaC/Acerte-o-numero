1b3eee5ddb441dcfad67385e8b0b2140bb0b216b
DevsousaC <devdiegosousa@gmail.com>
2024-02-16 14:35:21 -0300

    bug fixed
    Bug do numero 10 ficar sempre desabilitado foi corrigido

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
+// all menus of the game
 const menu = document.querySelector('div#menu')
-const playButton = document.querySelector('#play')
 const game = document.querySelector('#game')
-
+//buttons
+const playButton = document.querySelector('#play')
+const checkButton = document.querySelector('#check-button')
+// span
+const result = document.querySelector('#result')
+// correct number in the game
+// this number is always pseudorandom
+let randomNum = Math.floor(Math.random() * (10 - 1 + 1)) +1
 /* play game button */
 playButton.addEventListener('click', ()=>{
     menu.style.display = "none"
     game.style.display = "flex"
 })
-let randomNum = Math.floor(Math.random() * (10 - 1 + 1)) +1
-const checkButton = document.querySelector('#check-button')
-const result = document.querySelector('#result')
-let count = 0
-
+// game mechanics
 let selected = document.querySelectorAll('.number')
 selected.forEach(button => {
     button.addEventListener('click', evt =>{
-        for(count; count <3; count++){
+        for(let count = 0; count <3; count++){
             evt.target.disabled = true;
             if(evt.target.value == randomNum){
                 evt.target.style.backgroundColor ="var(--correct-color)"
@@ -43,15 +46,14 @@ selected.forEach(button => {
                    if (selected[position].value == randomNum) {
                     selected[position].style.backgroundColor ="var(--correct-color)"
                     break
-                    // this 'if' checks the button that contains the correct value and adds a green color to it
+                    // checks the button that contains the correct value and adds a green color to it
                    }
                 }
                 break
             }
         }   
     })
-} 
-)
+} )
 /* Play again button */
 const resetButton = document.querySelector('#reset-button') //new game button
 resetButton.addEventListener('click', ()=>{
@@ -61,12 +63,10 @@ resetButton.addEventListener('click', ()=>{
     result.innerHTML = ''; // reset the result text
     count = 0; // reset the chanche counter
     randomNum = Math.floor(Math.random() * (10 - 1 + 1)) + 1; // generates a new correct result
-    console.log(randomNum); // if you want to test all variables, you can see the correct result in console
-    for (let position = 0; selected[position].value < 10; position++) {
-        console.log(position)
+    for (let position = 0; selected[position].value <= 10; position++) {
         if (selected[position].disabled = true) {
          selected[position].disabled = false
-         // this 'if' checks the button that contains the correct value and adds a green color to it
+         // check which buttons are disabled and which are enabled
         }
      }
 })
